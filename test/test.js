44968e86bb9e6b555f2ab5b1fd2e8b955bd7bbcb
ELLIOTTCABLE <me@ell.io>
2015-03-31 16:56:00 -0800

    (new) Support strange indentations on the first line
    Now, if the first line is, for some reason, indented beyond other lines in the file, this module will still correctly de-indent the code.
    
    I tried to implement this as efficiently as possible, but this may still have performance ramifications.

--- test/test.js
+++ test/test.js
@@ -31,6 +31,22 @@ it("should unindent space-indented code", function () {
 	assert.equal(unindent(code), unindented);
 });
 
+it("should unindent to the level of the shallowest line", function () {
+	var code = [
+		'		a',
+		'	b',
+		'	  c'
+	].join('\n');
+	var unindented = [
+		'	a',
+		'b',
+		'  c'
+	].join('\n');
+
+	assert.equal(unindent(code), unindented);
+});
+
+
 it("should be able to convert leading tabs in code", function () {
 	var code = [
 		'	a',
